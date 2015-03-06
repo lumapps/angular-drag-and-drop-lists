3a8347d8a866639b3df1414477a714adafcb836c
Matthias Manoukian <matthias.manoukian@gmail.com>
2015-03-06 09:35:57 +0100

    Remove stop propagation on list click

--- angular-drag-and-drop-lists.js
+++ angular-drag-and-drop-lists.js
@@ -181,8 +181,6 @@ angular.module('dndLists', [])
           scope.$apply(function() {
             $parse(attr.dndSelected)(scope, {event: event});
           });
-
-          event.stopPropagation();
         }
       });
 
